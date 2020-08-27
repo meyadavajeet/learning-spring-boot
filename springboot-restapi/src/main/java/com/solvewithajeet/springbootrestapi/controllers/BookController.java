6530ee5c37b3409cd8e197dd61219dffe3b419a6
Ajeet Yadav <meyadavajeet@gmail.com>
2020-08-27 21:45:30 +0530

    after put and delete method impelemented

--- springboot-restapi/src/main/java/com/solvewithajeet/springbootrestapi/controllers/BookController.java
+++ springboot-restapi/src/main/java/com/solvewithajeet/springbootrestapi/controllers/BookController.java
@@ -8,9 +8,11 @@ import com.solvewithajeet.springbootrestapi.entities.Book;
 import com.solvewithajeet.springbootrestapi.services.BookService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -19,21 +21,46 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping(value="/books")
+    /**
+     * Get All books handler
+     */
+    @GetMapping(value = "/books")
     public List<Book> getAllBooks() {
         return this.bookService.getAllBooks();
     }
 
+    /**
+     * Get single book respect to id handler
+     */
     @GetMapping(value = "/book/{id}")
-    public Book getBookById(@PathVariable("id") int id){
+    public Book getBookById(@PathVariable("id") int id) {
         return bookService.getBookById(id);
     }
 
-    @PostMapping(value="/books")
+    /**
+     * create new book
+     * 
+     * @param book
+     * @return
+     */
+    @PostMapping(value = "/books")
     public Book addBook(@RequestBody Book book) {
-        Book b = this.bookService.addBook(book);
-        return b;
+     Book books = this.bookService.addBook(book);
+     return books;
     }
-    
+/**
+ * delete book respect to id
+ * @param bookId
+ */
+    @DeleteMapping(value = "/books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bookId) {
+        this.bookService.deleteBook(bookId);
+    }
+
+    @PutMapping(value ="/books/{bookId}")
+    public Book updateBook(@RequestBody Book book, @PathVariable("bookId") int bookId){
+        this.bookService.updateBook(book, bookId);
+        return book;
+    }
 
 }
--- springboot-restapi/src/main/java/com/solvewithajeet/springbootrestapi/services/BookService.java
+++ springboot-restapi/src/main/java/com/solvewithajeet/springbootrestapi/services/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 // @Service
@@ -13,38 +14,60 @@ public class BookService {
 
     private static List<Book> list = new ArrayList<>();
 
-    static{
-        list.add(new Book(12,"Complete java reference", "XYZ"));
-        list.add(new Book(13,"Complete Python reference", "abc"));
-        list.add(new Book(14,"Complete php reference", "lmn"));
-        list.add(new Book(14,"Complete mysql reference", "ajeet"));
+    static {
+        list.add(new Book(12, "Complete java reference", "XYZ"));
+        list.add(new Book(13, "Complete Python reference", "abc"));
+        list.add(new Book(14, "Complete php reference", "lmn"));
+        list.add(new Book(141, "Complete mysql reference", "ajeet"));
     }
 
     /**
-     * get all books 
-     * return book
+     * get all books return book
      */
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return list;
 
     }
 
     /**
-     * get single book by id
-     * return book
+     * get single book by id return book
      */
-    public Book getBookById(int id){
+    public Book getBookById(int id) {
         Book book = null;
-        book = list.stream().filter(e->e.getId()==id).findFirst().get();
+        book = list.stream().filter(e -> e.getId() == id).findFirst().get();
         return book;
     }
+
     /**
-     * post book and add in the array list 
+     * post book and add in the array list
      */
-    public Book addBook(Book book){
+    public Book addBook(Book book) {
         list.add(book);
         return book;
     }
 
-    
+    /**
+     * delete book with respect with id
+     * 
+     * @param bid
+     */
+    public void deleteBook(int bid) {
+        list.stream().filter(book -> book.getId() != bid).collect(Collectors.toList());
+    }
+
+    /**
+     * update book w.r.t bookId
+     * 
+     * @param book
+     * @param bookId
+     */
+    public void updateBook(Book book, int bookId) {
+        list.stream().map(b -> {
+            if (b.getId() == bookId) {
+                b.setTitle(book.getTitle());
+                b.setAuthor(book.getAuthor());
+            }
+            return b;
+        }).collect(Collectors.toList());
+    }
 }
