4d11c560eb3ed83c4cd8b0ce366cdf5f66ed8843
Ajeet Yadav <meyadavajeet@gmail.com>
2020-12-25 18:33:55 +0530

    rest api for the Employee all files

---
+++ rest-api-for-flutter/src/main/java/com/technicalajeet/restapiforflutter/RestApiForFlutterApplication.java
@@ -0,0 +1,13 @@
+package com.technicalajeet.restapiforflutter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForFlutterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiForFlutterApplication.class, args);
+    }
+
+}
